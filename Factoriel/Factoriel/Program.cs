6b9d795508f87abe9dca6f57966cd73e3988c0e4
Tak1Maker <salim.mouttaki@proton.me>
2023-10-18 14:23:59 +0200

    Initial commit

---
+++ Factoriel/Factoriel/Program.cs
@@ -0,0 +1,53 @@
+﻿
+//Recursive
+static ulong CalculerFactorielle(ulong n)
+{
+    if (n < 0)
+        throw new ArgumentException("La factorielle n'est définie que pour les nombres positifs.");
+    else if (n == 0)
+        return 1;
+    else
+    {
+        ulong resultat = 1;
+        for (ulong i = 1; i <= n; i++)
+        {
+            resultat *= i;
+        }
+        return resultat;
+    }
+}
+
+ulong n = 50;
+ulong resultat = CalculerFactorielle(n);
+Console.WriteLine($"La factorielle de {n} est {resultat}");
+
+ulong Recursive(ulong n)
+{
+    if (n == 1)
+        return 1;
+    else
+    {
+        return n* Recursive(n - 1);
+    }
+}
+
+Console.WriteLine(Recursive(30));
+
+
+int fct1(int n)
+{
+    if(n == 1)
+        return n * n;
+    else
+    {
+        return n *= (n - 1);
+    }
+}
+
+Console.WriteLine(fct1(5));
+
+int fct2(int n)
+{
+    if( n == 1)
+        return 
+}
